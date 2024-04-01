e4809e398bc1234d40cd43e66ab881e08da91b1a
Darren Liew <darrenliew96@gmail.com>
2024-04-01 22:35:55 +0800

    adding notification dropdown menu

--- src/app/components/backoffice/Navbar.jsx
+++ src/app/components/backoffice/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Sun, Bell, User, AlignJustify } from "lucide-react";
+import { Sun, Bell, User, AlignJustify, X } from "lucide-react";
 import Image from "next/image";
 import {
   DropdownMenu,
@@ -24,16 +24,73 @@ export default function Navbar() {
         <button>
           <Sun className="text-green-600" />
         </button>
-        <button
-          type="button"
-          className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-transparent rounded-lg"
-        >
-          <Bell className="text-green-600" />
-          <span className="sr-only">Notifications</span>
-          <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 rounded-full -top-0 -start-0 dark:border-gray-900">
-            20
-          </div>
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <button
+              type="button"
+              className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-transparent rounded-lg"
+            >
+              <Bell className="text-green-600" />
+              <span className="sr-only">Notifications</span>
+              <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 rounded-full -top-0 -start-0 dark:border-gray-900">
+                20
+              </div>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="py-2 px-4 pr-8">
+            <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <div className="flex items-center space-x-2">
+                <Image
+                  src="/profile.jpeg"
+                  alt="User Profile"
+                  width={200}
+                  height={200}
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="flex flex-col space-y-1">
+                  <p>Yellow Sweet Corn</p>
+                  <div className="flex items-center space-x-2">
+                    <p className="px-3 py-0.1 bg-red-700 text-white rounded-full text-sm">
+                      Stock Out
+                    </p>
+                    <p>2014-05-06</p>
+                  </div>
+                </div>
+                <button>
+                  <X />
+                </button>
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <div className="flex items-center space-x-2">
+                <Image
+                  src="/profile.jpeg"
+                  alt="User Profile"
+                  width={200}
+                  height={200}
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="flex flex-col space-y-1">
+                  <p>Yellow Sweet Corn</p>
+                  <div className="flex items-center space-x-2">
+                    <p className="px-3 py-0.1 bg-red-700 text-white rounded-full text-sm">
+                      Stock Out
+                    </p>
+                    <p>2014-05-06</p>
+                  </div>
+                </div>
+                <button>
+                  <X />
+                </button>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Image
